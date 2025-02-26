60ba8928708d3e4a8b7129807b44b8fa25beb61e
phonnz <gonzalezm.alfonso@gmail.com>
2025-02-26 20:44:53 +0100

    Add event_store as supervised child

--- lib/coin_exchange/application.ex
+++ lib/coin_exchange/application.ex
@@ -17,7 +17,8 @@ defmodule CoinExchange.Application do
       # Start a worker by calling: CoinExchange.Worker.start_link(arg)
       # {CoinExchange.Worker, arg},
       # Start to serve requests, typically the last entry
-      CoinExchangeWeb.Endpoint
+      CoinExchangeWeb.Endpoint,
+      CoinExchange.EventStore
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
