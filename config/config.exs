6dc4bfc46ff47a148f0ceccd23356f9c9ece5c81
phonnz <gonzalezm.alfonso@gmail.com>
2025-02-26 20:42:33 +0100

    Config eventstore

--- config/config.exs
+++ config/config.exs
@@ -61,6 +61,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :coin_exchange, event_stores: [CoinExchange.EventStore]
+# default_database: "coin_exchange_eventstore",
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
